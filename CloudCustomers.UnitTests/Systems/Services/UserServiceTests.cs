cc5624807efc52bd5169e8be5359afc532247417
gabrielkf <gabrielkf@gmail.com>
2022-09-27 11:48:22 -0300

    test GetAllUsersAsync return type

--- CloudCustomers.UnitTests/Systems/Services/UserServiceTests.cs
+++ CloudCustomers.UnitTests/Systems/Services/UserServiceTests.cs
@@ -15,23 +15,36 @@ namespace CloudCustomers.UnitTests.Systems.Services;
 
 public class UserServiceTests
 {
+    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+    private readonly HttpClient _mockHttpClient;
+    
+    public UserServiceTests()
+    {
+        _mockHttpMessageHandler = MockHttpMessageHandler
+            .SetupBasicGetResourceList<User>(UsersFixture.GetTestUsers());
+
+        _mockHttpClient = new HttpClient(_mockHttpMessageHandler.Object);
+    }
+
     [Fact]
     public async Task GetAllUserAsync_WhenCalled_InvokesHttpGetRequest()
     {
-        var expectedResponse = UsersFixture.GetTestUsers();
-        var mockHttpMessageHandler = MockHttpMessageHandler
-            .SetupBasicGetResourceList<User>(expectedResponse);
-        var mockHttpClient = new HttpClient(mockHttpMessageHandler.Object);
-        var sut = new UsersService(mockHttpClient);
-        
+        var sut = new UsersService(_mockHttpClient);
         var result = await sut.GetAllUsersAsync();
-
-        mockHttpMessageHandler
+        _mockHttpMessageHandler
             .Protected()
             .Verify(
                 Constants.SendAsyncMethodName,
                 Times.Exactly(1),
                 ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
                 ItExpr.IsAny<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task GetAllUsersAsync_ReturnsListOfUsers()
+    {
+        var sut = new UsersService(_mockHttpClient);
+        var result = await sut.GetAllUsersAsync();
+        result.Should().BeOfType<List<User>>();
     }
 }
